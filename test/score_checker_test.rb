f878d86c9269c299a7a70ccc4b1ea78c88a11bc7
squee <squeemishly@gmail.com>
2019-05-15 19:11:30 -0600

    clean up tests

--- test/score_checker_test.rb
+++ test/score_checker_test.rb
@@ -2,17 +2,9 @@ require_relative './test_helper'
 require './lib/score_checker'
 
 class ScoreCheckerTest < Minitest::Test
-  attr_reader :sc
-
-  def setup
-    @sc = ScoreChecker.new
-  end
-
-  def test_it_exists
-    assert_instance_of ScoreChecker, sc
-  end
-
   def test_it_can_calculate_the_base_value_of_a_word
+    sc = ScoreChecker.new
+
     assert_equal 2, sc.calculate_score("as")
     assert_equal 9, sc.calculate_score("thing")
   end
