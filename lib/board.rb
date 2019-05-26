248435dc713ccfcbcd6b1d143dbff734227f8f64
squee <squeemishly@gmail.com>
2019-05-25 21:56:29 -0600

    refactor out logic

--- lib/board.rb
+++ lib/board.rb
@@ -2,23 +2,34 @@ require_relative 'cell'
 
 class Board
   attr_reader :board
-  
+
   def initialize
     @board = build
   end
 
   def build
-    rows = ("A".."Z").to_a
     grid = Array.new(15) { Array.new(15) { '' } }
-
-    15.times do |row|
-      15.times do |column|
-        cell_row = rows[row]
-        cell_column = column + 1
-        cell = Cell.new(cell_row, cell_column)
-        grid[row][column] = cell
-      end
-    end
-    grid
+    add_cells_to_grid(grid)
   end
+
+  private
+
+    def add_cells_to_grid(grid)
+      15.times do |row|
+        15.times do |column|
+          create_cells(row, column, grid)
+        end
+      end
+
+      grid
+    end
+
+    def create_cells(row, column, grid)
+      rows = ("A".."Z").to_a
+
+      cell_row = rows[row]
+      cell_column = column + 1
+      cell = Cell.new(cell_row, cell_column)
+      grid[row][column] = cell
+    end
 end
