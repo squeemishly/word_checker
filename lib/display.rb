a3c8cdcc11c98d7bc0f5e8aeb552c71b4472bab3
squee <squeemishly@gmail.com>
2019-05-26 07:56:12 -0600

    add row letters and column numbers
    this is hideous... :(

--- lib/display.rb
+++ lib/display.rb
@@ -8,16 +8,29 @@ class Display
   end
 
   def show_board
+    print "    "
+    for x in 1..8 do
+      print "#{x}   "
+    end
+
+    for x in 9..15 do
+      print "#{x}  "
+    end
+
+    print "\n"
+
+    print "  "
     print "-" * 61
-    print "\n|"
+    print "\n"
     board.board.each do |row|
+      print "#{row[0].row} |"
       row.each do |column|
         print " #{column.value} |"
       end
 
       print "\n"
+      print " " * 2
       puts "-" * 61
-      print "|" if row[0].row != "O"
     end
   end
 end
