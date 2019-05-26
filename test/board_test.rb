44631eb38b8f7ab6d079b24cc0c6aefd719e1ec9
squee <squeemishly@gmail.com>
2019-05-25 21:56:37 -0600

    add tests for board size

--- test/board_test.rb
+++ test/board_test.rb
@@ -4,8 +4,11 @@ require './lib/board'
 class BoardTest < Minitest::Test
   def test_it_creates_a_15_x_15_board
     board = Board.new
+    assert_equal 15, board.board.count
 
     board.board.each do |row|
+      assert_equal 15, row.count
+      
       row.each do |column|
         assert_instance_of Cell, column
       end
